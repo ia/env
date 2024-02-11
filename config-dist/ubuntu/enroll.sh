b6e7fae534a96985a9acc53fba499124450f9630
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-02-12 01:31:11 +0300

    config-dist/ubuntu: enroll.sh - add grub config update

--- config-dist/ubuntu/enroll.sh
+++ config-dist/ubuntu/enroll.sh
@@ -8,4 +8,7 @@ cp  etc_apt_apt.conf.d_00bloatware  /etc/apt/apt.conf.d/
 echo "====>>>> Setting repos for apt..."
 cp  etc_apt_sources.list  /etc/apt/sources.list
 
+echo "====>>>> Update grub config..."
+cp  etc_default_grub  /etc/default/grub
+
 # TBA
