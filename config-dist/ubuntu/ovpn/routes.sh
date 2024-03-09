ccab957b215d43940e93eee006b89f8b8443ed04
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-09 23:18:46 +0300

    config-dist/ubuntu: ovpn/routes.sh - add script with routing commands as documentational reference

---
+++ config-dist/ubuntu/ovpn/routes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+CLIENT="10.8.0.4"
+IFACE="tun1"
+TABLE=vpnclient
+
+ip  route  add  10.8.0.0/24               dev  ${IFACE}  src  10.8.0.1  table  vpnclient
+ip  route  add  default  via  10.8.0.254  dev  ${IFACE}                 table  vpnclient
+ip  rule   add  from  ${CLIENT}/32                                      table  vpnclient
+ip  rule   add  to    ${CLIENT}/32                                      table  vpnclient
+
+ip  route  flush  cache
+
