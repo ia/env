f9065c2b45f849c5e43b66c8d8b39bec9f604894
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-16 22:59:47 +0300

    config-dist/ubuntu: ovpn/routes.sh - add route table info

--- config-dist/ubuntu/ovpn/routes.sh
+++ config-dist/ubuntu/ovpn/routes.sh
@@ -6,6 +6,9 @@ CLIENT="10.8.0.11"
 IFACE="tun0"
 TABLE=vpnclient
 
+# one time command:
+# $ echo  "200 vpnclient"  |  sudo  tee  -a  /etc/iproute2/rt_tables
+
 ip  route  add  10.8.0.0/24               dev  ${IFACE}  src  10.8.0.1  table  vpnclient
 ip  route  add  default  via  10.8.0.254  dev  ${IFACE}                 table  vpnclient
 ip  rule   add  from  ${CLIENT}/32                                      table  vpnclient
