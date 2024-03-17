aab92e72e5c9e970505eb2a98224f71396305942
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-17 23:28:06 +0300

    config-dist/ubuntu: ovpn/routes.sh - rename table

--- config-dist/ubuntu/ovpn/routes.sh
+++ config-dist/ubuntu/ovpn/routes.sh
@@ -4,15 +4,15 @@ set -e
 
 CLIENT="10.8.0.11"
 IFACE="tun0"
-TABLE=vpnclient
+TABLE=vpn
 
 # one time command:
 # $ echo  "200 vpnclient"  |  sudo  tee  -a  /etc/iproute2/rt_tables
 
-ip  route  add  10.8.0.0/24               dev  ${IFACE}  src  10.8.0.1  table  vpnclient
-ip  route  add  default  via  10.8.0.254  dev  ${IFACE}                 table  vpnclient
-ip  rule   add  from  ${CLIENT}/32                                      table  vpnclient
-ip  rule   add  to    ${CLIENT}/32                                      table  vpnclient
+ip  route  add  10.8.0.0/24               dev  ${IFACE}  src  10.8.0.1  table  vpn
+ip  route  add  default  via  10.8.0.254  dev  ${IFACE}                 table  vpn
+ip  rule   add  from  ${CLIENT}/32                                      table  vpn
+ip  rule   add  to    ${CLIENT}/32                                      table  vpn
 
 ip  route  flush  cache
 
