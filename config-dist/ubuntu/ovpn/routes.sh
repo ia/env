55655ac353789bdbaaad6fa68f6c2e643b9192dc
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-17 23:28:48 +0300

    config-dist/ubuntu: ovpn/routes.sh - replace table in ip commmands by variable

--- config-dist/ubuntu/ovpn/routes.sh
+++ config-dist/ubuntu/ovpn/routes.sh
@@ -9,10 +9,10 @@ TABLE=vpn
 # one time command:
 # $ echo  "200 vpnclient"  |  sudo  tee  -a  /etc/iproute2/rt_tables
 
-ip  route  add  10.8.0.0/24               dev  ${IFACE}  src  10.8.0.1  table  vpn
-ip  route  add  default  via  10.8.0.254  dev  ${IFACE}                 table  vpn
-ip  rule   add  from  ${CLIENT}/32                                      table  vpn
-ip  rule   add  to    ${CLIENT}/32                                      table  vpn
+ip  route  add  10.8.0.0/24               dev  ${IFACE}  src  10.8.0.1  table  ${TABLE}
+ip  route  add  default  via  10.8.0.254  dev  ${IFACE}                 table  ${TABLE}
+ip  rule   add  from  ${CLIENT}/32                                      table  ${TABLE}
+ip  rule   add  to    ${CLIENT}/32                                      table  ${TABLE}
 
 ip  route  flush  cache
 
